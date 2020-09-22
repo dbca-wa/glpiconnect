3014adbab123e98a408b8b7279cfa7265c1caf86
Ashley Felton <ashley@ropable.com>
2020-09-22 13:48:06 +0800

    Redirect root URL to admin.

--- glpiconnect/urls.py
+++ glpiconnect/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/admin')),
 ]
